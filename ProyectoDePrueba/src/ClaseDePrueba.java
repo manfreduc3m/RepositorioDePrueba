7ddefb95471600e5860cdb142bdcbb39f5d595f2
JF Rascón Crespo <jfrascon@gmail.com>
2013-02-27 21:41:25 +0100

    Commit después de añadir ficheros de configuración.

--- ProyectoDePrueba/src/ClaseDePrueba.java
+++ ProyectoDePrueba/src/ClaseDePrueba.java
@@ -6,7 +6,7 @@ public class ClaseDePrueba {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Impresion de prueba.");
+		System.out.println("Impresion de prueba wow.");
 
 	}
 
