6bb017e1d65e04eaa6906edba298a3c5022a72fc
JF Rascón Crespo <jfrascon@gmail.com>
2013-02-27 21:22:31 +0100

    El primer commit de la prueba

---
+++ ProyectoDePrueba/src/ClaseDePrueba.java
@@ -0,0 +1,13 @@
+
+public class ClaseDePrueba {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Impresion de prueba.");
+
+	}
+
+}
